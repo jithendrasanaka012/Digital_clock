6cb6ac69473f206104082ea48167a07b17a57dce
jithendrasanaka012 <jithendrasanaka0123@gmail.com>
2024-10-17 01:18:41 +0530

    i am commited and changing my code

--- src/App.js
+++ src/App.js
@@ -20,11 +20,11 @@ class App extends React.Component {
       <>
         <div id="md">
 
-          <div className="text" id="by"><img src="Assets/images/newborn.jpg"></img> <h3>Birth</h3> </div>
-          <div className="text" id="m"><img src="Assets/images/landscape.jpg"></img><h3>Grown</h3></div>
-          <div className="text" id="ma"><img src="Assets/images/wedding.webp"></img><h3>Main Part</h3></div>
-          <div className="text" id="fa"><img src="Assets/images/family1.jpg"></img><h3>New Way</h3></div>
-          <div className="text" id="ce"><img src="Assets/images/cemetery.jpg"></img><h3>Tension Free  </h3></div>
+          <div className="text" id="by"><img src="Assets/images/newborn.jpg" alt="image"></img> <h3>Birth</h3> </div>
+          <div className="text" id="m"><img src="Assets/images/landscape.jpg" alt="image"></img><h3>Grown</h3></div>
+          <div className="text" id="ma"><img src="Assets/images/wedding.webp" alt="image"></img><h3>Main Part</h3></div>
+          <div className="text" id="fa"><img src="Assets/images/family1.jpg" alt="image"></img><h3>New Way</h3></div>
+          <div className="text" id="ce"><img src="Assets/images/cemetery.jpg" alt="image"></img><h3>Tension Free  </h3></div>
           
           <div id="mi">
           <h1>{this.state.time}</h1>
